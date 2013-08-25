83d409740a9951f0bc993753ae004a968ef7d420
Xiaocong He <xiaocong@gmail.com>
2013-08-25 22:31:37 +0800

    v0.1.6

--- uiautomator.py
+++ uiautomator.py
@@ -17,7 +17,7 @@ try:
 except ImportError:
     import urllib.request as urllib2
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __author__ = "Xiaocong He"
 __all__ = ["device", "rect", "point", "adb", "Selector"]
 
